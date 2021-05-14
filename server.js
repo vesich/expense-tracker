457d12280e01645dcd1a00e0eb29084b3f3d93fa
Veselin Georgiev <veselin.georgief@gmail.com>
2021-05-14 08:18:16 -0400

    some modifications made

--- server.js
+++ server.js
@@ -28,4 +28,5 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000
 
+
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
